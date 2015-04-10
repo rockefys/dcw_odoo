02dcc64f9c40dc866c03a5af75b1800e0190e0af
liangdachuwang <liang@dachuwang.com>
2015-04-10 14:45:03 +0800

    help模块添加概述财务等菜单

--- openerp/addons/help/help.py
+++ openerp/addons/help/help.py
@@ -9,12 +9,12 @@ class wms_help(osv.osv):
         'date_upd':fields.date(u'更新日期'),
 	'content':fields.html(u'内容'),
 	'author':fields.char(u'作者',size=50,select=True),
-	'type':fields.selection([('purchase','采购'),('input','入库'),('output','出库')],'分类',required=True),
+	'type':fields.selection([('summary','概述'),('purchase','采购'),('input','入库'),('output','出库'),('return','退货'),('finance','财务'),('check','盘点'),('other','其他')],'分类',required=True),
     }
 
     _defaults = {
-        'type' : 'purchase',
-	'date_upd':fields.datetime.now,
+        'type' : 'summary',
+        'date_upd':fields.datetime.now,
     }
  
 wms_help()
